810e020c2fa67d7878f81c118760cf25823772b6
ashishhpdave <141114347+ashishhpdave@users.noreply.github.com>
2024-09-02 22:22:42 +0530

    All-Data-Display-In-Right-Sidebar
    This s code and funcnelity for all item and userData and bio and our profile DP and title and image display in rightSidebar

--- src/components/RightSidebar/RightSidebar.jsx
+++ src/components/RightSidebar/RightSidebar.jsx
@@ -1,29 +1,51 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import './RightSidebar.css'
 import assets from '../../assets/assets'
 import { logout } from '../../config/firebase'
+import { AppContext } from '../../context/AppContext'
 
 const RightSidebar = () => {
-  return (
+
+  const {chatUser,messages} = useContext(AppContext);
+  const [msgImages,setMsgImages] = useState([]);
+
+
+  useEffect(()=>{
+    let tempVar = [];
+    messages.map((msg)=>{
+      if (msg.image) {
+        tempVar.push(msg.image)
+      }
+    })
+    setMsgImages(tempVar);
+  },[messages])
+
+  return chatUser ? (
     <div className='rs'>
         <div className="rs-profile">
-            <img src={assets.profile_img} alt="" />
-            <h3>Richard Sanford <img src={assets.green_dot} className='dot' alt="" /></h3>
-            <p>Hey, There i am Richard Sanford useing chat app</p>
+            <img src={chatUser.userData.avatar} alt="" />
+            <h3> {chatUser.userData.name} <img src={assets.green_dot} className='dot' alt="" /></h3>
+            <p>{chatUser.userData.bio}</p>
         </div>
         <hr />
         <div className="rs-media">
             <p>Media</p>
             <div>
-                <img src={assets.pic1} alt="" />
+              {msgImages.map((url,index)=>(<img key={index} src={url} alt=''/>))}
+                {/* <img src={assets.pic1} alt="" />
                 <img src={assets.pic2} alt="" />
                 <img src={assets.pic3} alt="" />
                 <img src={assets.pic4} alt="" />
                 <img src={assets.pic1} alt="" />
-                <img src={assets.pic2} alt="" />
+                <img src={assets.pic2} alt="" /> */}
             </div>
         </div>
         <button onClick={()=>logout()}>Logout</button>
+    </div>
+  )
+  : (
+    <div className='rs'>
+      <button onClick={()=>logout()}>Logout</button>
     </div>
   )
 }
